a141af1d68073ee2be8c12486bd0e7cebca93e9e
Taghizadeh Alamdari <nxt170030@ee87437.utdallas.edu>
2019-04-15 16:02:26 -0500

    Uploaded the iOS version of the app

---
+++ PersonalizedNR-iOS/PersonalizedNR_App/PersonalizedNR_App-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  PersonalizedNR_App-Bridging-Header.h
+//  PersonalizedNR_App
+//
+//  Created by Taghizadeh Alamdari, Nasim on 3/26/19.
+//  Copyright © 2019 UT Dallas. All rights reserved.
+//
+
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "IosAudioController.h"
